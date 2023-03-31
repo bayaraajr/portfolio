7499a04232a2662427f0a201818ade633c740c5b
Bayarjargal Jargalsaikhan <gl.bayaraa@gmail.com>
2023-03-31 10:31:50 +0000

    Update pages 📄

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout';
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <Layout>
+            <h1>About</h1>
+        </Layout>
+    )
+}
+
+export default Projects;
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout';
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <Layout>
+            <h1>Contact</h1>
+        </Layout>
+    )
+}
+
+export default Projects;
